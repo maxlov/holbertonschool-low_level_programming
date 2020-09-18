339c34b4acca7247342ac4ffe587db3a334e7a94
JohnBogey <maxwellloganlovell@gmail.com>
2020-09-18 17:40:47 +0000

    prints string using system() into stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,18 @@
 /**
  * main - prints a line with system echo
- * Return: returns 0
+ * Return: returns 1
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main(void)
+int main()
 {
 	char command[100];
 
 	strcpy(command, "echo 'and that piece of art is useful\" \
-- Dora Korpar, 2015-10-19'");
+- Dora Korpar, 2015-10-19' >&2");
 	system(command);
-	return (0);
+	return (1);
 }
