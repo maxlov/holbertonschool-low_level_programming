b556be932084c96e58d90c33fcf6cccaed1e2019
JohnBogey <maxwellloganlovell@gmail.com>
2020-09-22 17:23:53 +0000

    prints last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,11 @@
 
 int print_last_digit(int n)
 {
-	n = n % 10;
-	return (n);
+	int last_digit;
+
+	last_digit = n % 10;
+	if (last_digit < 0)
+		last_digit = last_digit * -1;
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
