be017cc3b48d053c3109f56a4149923c5915f8f7
JohnBogey <maxwellloganlovell@gmail.com>
2020-09-22 13:21:15 +0000

    header file, readme, and task 0 which prints holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - uses _putchar to print hoblerton.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char holberton[10] = "holberton\n";
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+	_putchar(holberton[n]);
+	}
+	return (0);
+}
