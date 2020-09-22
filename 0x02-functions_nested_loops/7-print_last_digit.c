21868639806aca6dcc45314aba12263026349fd1
JohnBogey <maxwellloganlovell@gmail.com>
2020-09-22 14:57:02 +0000

    returns last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - returns last digit of an integer
+ * @n: a number
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
