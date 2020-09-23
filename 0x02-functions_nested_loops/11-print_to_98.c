01bb05c95a8d32c960d97dea23cf7a979158deca
JohnBogey <maxwellloganlovell@gmail.com>
2020-09-23 18:02:31 +0000

    increments or decrements numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,9 +12,9 @@ void print_to_98(int n)
 	while (n != 98)
 	{
 		if (n < 98)
-                        printf("%d, ", n++);
+			printf("%d, ", n++);
 		if (n > 98)
 			printf("%d, ", n--);
 	}
-        printf("%d\n", n);
+	printf("%d\n", n);
 }
