66de19c2c8786880e31c6ab68648d02089392d30
JohnBogey <maxwellloganlovell@gmail.com>
2020-09-24 18:55:25 +0000

    checks for digit 0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - checks if character is a digit 0-9
+ * @c: number
+ *
+ * Return: 1 if digit 0 if otherwise
+ */
+
+int _isdigit(int c);
+{
+	int output, number;
+
+	output = 0;
+	for (number = '0'; number <= '9'; number++)
+		if (number == c)
+			output = 1;
+	return (output);
+}
