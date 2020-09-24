5c760017c00788ea33779d30ca11478a52b1bbaf
JohnBogey <maxwellloganlovell@gmail.com>
2020-09-24 18:12:02 +0000

    prints a triangle based on given size

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - triangle in the terminal
+ * @size: size of triangle
+ * Return: nothing.
+ */
+
+void print_triangle(int size)
+{
+	int space, pound, row;
+
+	if (size > 0)
+		for (row = 1; row <= size; row++)
+		{
+			for (space = (size - row); space >= 0; space--)
+				_putchar(' ');
+			for (pound = 1; pound <= row; pound++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	else
+		 _putchar('\n');
+}
