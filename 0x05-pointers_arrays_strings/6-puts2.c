81e3c5676d2204852589919a678dc8b6596b553d
JohnBogey <maxwellloganlovell@gmail.com>
2020-09-29 17:21:59 +0000

    prints every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - outputs string
+ * @str: a string
+ *
+ * Return: length of string
+ */
+void puts2(char *str)
+{
+	int count;
+
+	for (count = 2; *str != '\0'; str++)
+	{
+		if ((count % 2) == 0)
+			_putchar(*str);
+		count++;
+	}
+       	_putchar('\n');
+}
