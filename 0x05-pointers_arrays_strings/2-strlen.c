ae312d0c3e69976224ca8a7918380d38ccfe57d8
JohnBogey <maxwellloganlovell@gmail.com>
2020-09-29 13:26:35 +0000

    returns length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: a string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int string_length = 0;
+
+	for (; *s != '\0'; s++)
+		string_length++;
+	return(string_length);
+}
