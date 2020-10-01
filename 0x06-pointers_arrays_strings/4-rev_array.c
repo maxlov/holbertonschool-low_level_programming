de856e4b4c2c419e7ef3c4c75db05cd0416d41a8
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-01 17:41:35 +0000

    reverses array of n size

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: array
+ * @n: int
+ *
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int pos = 0;
+	char temp;
+
+	while (pos <= n)
+	{
+		n--;
+		temp = a[pos];
+		a[pos] = a[n];
+		a[n] = temp;
+		pos++;
+	}
+}
