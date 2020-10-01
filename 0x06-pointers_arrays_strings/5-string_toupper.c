b073c7f224385b14a9f69f29b9280e7467f179c1
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-01 18:24:17 +0000

    lowercase to capitals

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes all lowercase to upper
+ * s: string
+ *
+ * Return: nothing
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] -= 32;
+	return (s);
+}
