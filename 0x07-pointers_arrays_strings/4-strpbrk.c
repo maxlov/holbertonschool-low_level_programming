694d9e3238a44868f882ce2d56b5a13417632a0d
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-05 19:56:04 +0000

    searches string for set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strchr - returns pointer to the first occur
+ * @s: pointer
+ * @c: character
+ *
+ * Return: string at character
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+		if (!*s++)
+			return (NULL);
+	return (s);
+}
+
+/**
+ * _strpbrk - finds first occurance in string s of any bytes in string accept
+ * @s: string
+ * @accept: string with characters being checked for
+ *
+ * Return: pointer to memory area s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while(*s)
+		if(_strchr(accept, *s++))
+			return (char*)--s;
+	return (0);
+}
