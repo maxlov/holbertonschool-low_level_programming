4ebae3de6eb5e9c7ec208b7ef1b689f620d684ae
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-06 18:51:29 +0000

    advanced file and diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints sums of diagonals
+ * @a: an array
+ * @size: size of matrix
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1, sum2;
+
+	for (i = 0; i <= (size * size) - 1; i += size + 1)
+		sum1 += a[i];
+	for (i = size * (size - 1); i > 0; i -= size - 1)
+		sum2 += a[i];
+	printf("%d, %d\n", sum1, sum2);
+}
