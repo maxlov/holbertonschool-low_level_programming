a218002b9c21d6371a6cc87a53744d7ad171142d
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-06 19:01:02 +0000

    added 0's

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+101-crackme_password#include "holberton.h"
 #include <stdio.h>
 /**
  * print_diagsums - prints sums of diagonals
@@ -9,7 +9,7 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, sum1, sum2;
+	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < (size * size); i += (size + 1))
 		sum1 += a[i];
