106b90e66d07709ff59e5dbeb023de9b25dc79e9
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-06 18:58:57 +0000

    fixed so bigger and negatives work

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,9 +11,9 @@ void print_diagsums(int *a, int size)
 {
 	int i, sum1, sum2;
 
-	for (i = 0; i < (size * size); i += size + 1)
+	for (i = 0; i < (size * size); i += (size + 1))
 		sum1 += a[i];
-	for (i = size * (size - 1); i > 0; i -= size - 1)
+	for (i = size * (size - 1); i > 0; i -= (size - 1))
 		sum2 += a[i];
 	printf("%d, %d\n", sum1, sum2);
 }
