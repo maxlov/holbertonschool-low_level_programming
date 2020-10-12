8fe281691ccc70ed1fa8b7dcafd7bc74baf0659b
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-12 00:40:11 +0000

    prints all arguments received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed into program
+ * @argc: number of strings called
+ * @argv: pointer to words
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	while (*argv)
+		printf ("%s\n", *argv++);
+	return (0);
+}
