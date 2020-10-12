5851a95abe24be018d821ff08283f3b9a9508fca
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-12 18:15:20 +0000

    script that gives number of coins needed for change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - counts number of coins needed for change
+ * @argc: number of strings
+ * @argv: array of strings
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int change = atoi(argv[1]), coin = 0, i = 0;
+	int coin_val[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argc < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (change > 0)
+	{
+		if (change >= coin_val[i])
+		{
+			change -= coin_val[i];
+			coin++;
+			i = 0;
+		}
+		else
+			i++;
+	}
+	printf("%d\n", coin);
+	return (0);
+}
