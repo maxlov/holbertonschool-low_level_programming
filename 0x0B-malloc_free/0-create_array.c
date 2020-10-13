7e789500230b2f2d64f9c7ea7fcbf5a3ad17866a
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-13 17:28:42 +0000

    fix extra null byte

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,11 +17,10 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	array = malloc((size + 1) * sizeof(char));
+	array = malloc((size) * sizeof(char));
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		array[i] = c;
-	array[i] = '\0';
 	return (array);
 }
