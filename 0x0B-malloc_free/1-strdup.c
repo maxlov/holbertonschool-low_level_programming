0a7df6951968669d85225793146a98e9eab9b72f
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-13 17:44:16 +0000

    fix missing null checks

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,9 +14,13 @@ char *_strdup(char *str)
 	char *duplicate;
 	int size = 0, i;
 
+	if (str == NULL)
+		return (NULL);
 	while (str[size] != '\0')
 		size++;
 	duplicate = malloc((size + 1) * sizeof(char));
+	if (duplicate == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		duplicate[i] = str[i];
 	duplicate[i] = '\0';
