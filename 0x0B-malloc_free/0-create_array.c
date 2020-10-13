d1c0e823a93778a6596855c109dcee3915fa21f4
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-13 17:18:17 +0000

    fix not returning null when fails

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,6 +18,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc((size + 1) * sizeof(char));
+	if (array == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		array[i] = c;
 	array[i] = '\0';
