75fb89831f81ab6eda5e9a50ee11ff3856c7b4ea
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-13 21:05:38 +0000

    frees memory allocated for grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * free_grid - duplicated string
+ * @grid: 2d grid
+ * @height: number of rows
+ *
+ * Return: pointer to array
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i <= height; i++)
+		free(grid[i]);
+}
