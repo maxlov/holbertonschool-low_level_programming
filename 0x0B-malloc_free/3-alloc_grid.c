159a71c9d613f5c024d84c9d9183d5d8019eeaad
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-13 19:55:12 +0000

    returns pointer to a 2d array of ints

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * alloc_grid - duplicated string
+ * @width: # of columns
+ * @height: # of rows
+ *
+ * Return: pointer to array
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j, c;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	arr = malloc(height * sizeof(int *));
+	if(!arr)
+		return (NULL);
+	for (c = 0; c <= width; c++)
+		arr[c] = malloc(width * sizeof(int *));
+	for (i = 0; i < width; i++)
+		for (j = 0; j < height; j++)
+			arr[i][j] = 0;
+	return (arr);
+}
