09a67734fe82b871268d1dd16965a509a0eb2de8
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-16 18:01:19 +0000

    reallocates pointer mem

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "holberton.h"
+/**
+ *_realloc - reallocates mem
+ *@ptr: pointer
+ *@old_size: original pointer size
+ *@new_size: new pointer size
+ *
+ *Return: reallocated pointer
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i = 0;
+	char *reallocated_pointer, *ptr_char = ptr;
+
+	if (new_size == old_size)
+		return(ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	if (new_size < old_size)
+		old_size = new_size;
+	reallocated_pointer = malloc(new_size);
+	for (i = 0; i < old_size; i++)
+		reallocated_pointer[i] = ptr_char[i];
+	free(ptr);
+	return (reallocated_pointer);
+}
