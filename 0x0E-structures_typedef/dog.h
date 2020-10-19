706a7fa88752884061bbc9b9ab1ebb15732d4484
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-19 19:13:45 +0000

    made new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef dog_h
 #define dog_h
+
 /**
  * struct dog - Information for a dog
  * @name: Dog's name
@@ -15,6 +16,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
