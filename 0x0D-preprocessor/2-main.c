dceda4229dc9ec08428b1f464679889ad27ae261
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-19 17:00:52 +0000

    tasks, readme, and header

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints file it was compiled from
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef four
+#define four
+#define ABS(x) (((x) < 0) ? (-(x)) : (x))
+#endif
