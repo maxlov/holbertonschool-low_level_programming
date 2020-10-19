d5984edbcd90ff14f3a03f4c473c1e522780e465
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-19 17:33:18 +0000

    task 0, readme, and header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - Information for a dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner's name
+ *
+ * Description: A structure that contains a dogs's name, age, and owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
