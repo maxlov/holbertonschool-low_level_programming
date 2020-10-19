66c3bf10dd2ee92e78f7cb044b158d482c58d14f
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-19 18:01:55 +0000

    inits variable of type dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,8 +10,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL || name == NULL || owner == NULL)
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return (0);
 }
