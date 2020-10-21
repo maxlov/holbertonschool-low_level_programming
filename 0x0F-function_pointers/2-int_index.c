6e9f967510207d3865668bd3e7eb7d007d5a0990
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-21 17:16:53 +0000

    searches for integer based on parameter

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for integer based on given param
+ * @array: array of numbers
+ * @size: number of elems in array
+ * @cmp: pointer to which func to use
+ *
+ * Return: nothing
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if ((*cmp)(array[i]))
+			return (i);
+	return (0);
+}
