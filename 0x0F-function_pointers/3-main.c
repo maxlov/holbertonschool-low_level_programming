736700de92190b42931d4d875b2892cf00ab3c4f
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-22 02:09:32 +0000

    testing if 2

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,11 +22,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-/*	if (argv[2][1] != '\0' || get_op_func(argv[2]) == NULL)
+	if (argv[2][1] != '\0' /*|| get_op_func(argv[2]) == NULL*/)
 	{
 		printf("Error\n");
 		exit(99);
-		}*/
+	}
 	if (((symbol == '/') || (symbol == '%')) && n2 == 0)
 	{
 		printf("Error\n");
