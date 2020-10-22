14f5607b0cdf822717a9c50e63eedc3c53c4ad99
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-22 01:56:26 +0000

    fixed order

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,25 +14,25 @@ int main(int argc, char *argv[])
 	int result, n1, n2;
 	char symbol;
 
+	n1 = atoi(argv[1]);
+        n2 = atoi(argv[3]);
+        symbol = argv[2][0];
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-	symbol = argv[2][0];
-	if (((symbol == '/') || (symbol == '%')) && n2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	result = get_op_func(argv[2])(n1, n2);
 	if (argv[2][1] != '\0' || get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	if (((symbol == '/') || (symbol == '%')) && n2 == 0)
+        {
+                printf("Error\n");
+                exit(100);
+        }
+	result = get_op_func(argv[2])(n1, n2);
 	printf("%d\n", result);
 	return (0);
 }
