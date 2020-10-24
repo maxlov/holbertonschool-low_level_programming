44193e62238618586de39454ee15b0972cee125b
JohnBogey <maxwellloganlovell@gmail.com>
2020-10-24 18:19:16 +0000

    fix missing null check

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,13 +15,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list string_list;
 	unsigned int i;
+	char * string;
 
 	va_start(string_list, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%p", va_arg(string_list, char *));
+		string = va_arg(string_list, char *);
 		if (i < n - 1 && separator != NULL)
-			printf("%s", separator);
+			printf("%s", string);
+		else
+			printf("%p", string);
 	}
 	printf("\n");
 	va_end(string_list);
