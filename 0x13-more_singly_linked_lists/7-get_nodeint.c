66bef8c968f773f6ea46652f2d677d6ffd11b875
JohnBogey <maxwellloganlovell@gmail.com>
2020-11-03 15:57:10 +0000

    04 code_fix line order for 7 and code checks for 9

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,13 +16,16 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current;
 	unsigned int current_index = 0;
 
+	if (head == NULL)
+		return (NULL);
+
 	current = head;
 	while (current->next != NULL)
 	{
-		current_index++;
-		current = current->next;
 		if (current_index == index)
 			return (current);
+		current_index++;
+		current = current->next;
 	}
 	return (NULL);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,6 +4,26 @@
 #include "lists.h"
 
 /**
+ * listint_len - counts number of elements in a list
+ * @h: head of list
+ *
+ * Return: # of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *scout = h;
+	size_t node_count = 0;
+
+	while (scout != NULL)
+	{
+		node_count++;
+		scout = scout->next;
+	}
+	return (node_count);
+}
+
+/**
  * get_nodeint_at_index - returns node at given index
  * @head: the list
  * @index: index of the node starting at 0
@@ -28,6 +48,27 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 }
 
 /**
+ * add_nodeint - adds node to list
+ * @head: the list
+ * @n: a number
+ *
+ * Return: Address of new node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
+
+/**
  * insert_nodeint_at_index - inserts a node at index
  * @head: the list
  * @idx: index
@@ -39,8 +80,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp_head, *current, *temp, *new;
+	unsigned int len = 0;
 
 	temp_head = *head;
+	len = listint_len(temp_head);
+	if (head == NULL || idx == len)
+		return (add_nodeint_end(head, n));
+	if (idx == 0)
+		return (add_nodeint(head, n));
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
