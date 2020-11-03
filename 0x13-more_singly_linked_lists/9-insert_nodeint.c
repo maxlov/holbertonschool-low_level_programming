c6e6067152662a4bc572d56c0aa1b7aa9f8e4dd8
JohnBogey <maxwellloganlovell@gmail.com>
2020-11-03 21:31:54 +0000

    11 code_add another edge case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -98,7 +98,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->next = NULL;
 	temp = get_nodeint_at_index(temp_head, idx);
 	current = get_nodeint_at_index(temp_head, idx - 1);
-	if (current == NULL)
+	if (current == NULL || temp == NULL)
 	{
 		free(new);
 		return (NULL);
