9c0dcf014315b5c462d49eeff113343006b47145
JohnBogey <maxwellloganlovell@gmail.com>
2020-11-04 22:28:21 +0000

    14 code_add advanced task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements in a list
+ * @head: A list
+ *
+ * Return: pointer to first node of list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next, *prev;
+
+	next = NULL;
+	prev = NULL;
+	while ((*head)->next != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = (*head);
+		(*head) = next;
+	}
+	*head = prev;
+	return (*head);
+}
