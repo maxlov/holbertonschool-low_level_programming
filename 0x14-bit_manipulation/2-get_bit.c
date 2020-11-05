c22d4d6280c896cbfaeaced2f9659cd2da7f2065
JohnBogey <maxwellloganlovell@gmail.com>
2020-11-05 18:53:41 +0000

    05 code_add task 2 get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,20 +3,6 @@
 #include "holberton.h"
 
 /**
- * _log2 - finds log2 of number
- * @n: a number
- *
- * Return: answer
- */
-unsigned int _log2(unsigned int n)
-{
-	if (n > 1)
-		return (1 + _log2(n / 2));
-	else
-		return (0);
-}
-
-/**
  * get_bit - gets bit at position index
  * @n: a number
  * @index: position
@@ -28,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit = n >> index;
 
-	if (((_log2(n) + 1) < index) || index >= 64)
+	if (index >= 64)
 		return (-1);
 	return (bit & 1);
 }
