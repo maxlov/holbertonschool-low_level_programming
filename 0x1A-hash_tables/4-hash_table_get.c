d47748504e2762649fc6ef74448e63bf5819d957
JohnBogey <maxwellloganlovell@gmail.com>
2021-01-15 19:15:10 +0000

    removed some null checks

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *current;
 
-	if (key == NULL || ht == NULL || !(*key))
+	if (key == NULL || ht == NULL)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
