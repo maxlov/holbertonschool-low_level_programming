6842c2ff61d88d082c029c1077fa257d0cfd6b67
JohnBogey <maxwellloganlovell@gmail.com>
2021-01-15 19:29:51 +0000

    code_fix missing plans for errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,39 +13,36 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new;
-	char *val_clone;
-
-	/*test to see if checker issues are errors*/
-	return (0);
-
+	hash_node_t *new, *current;
 
 	if (key == NULL)
 		return (0);
 
-	val_clone = strdup(value);
-	if (val_clone == NULL)
-	{
-		free(val_clone);
-		return (0);
-	}
-
 	index = key_index((const unsigned char *)key, ht->size);
+
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
-	{
-		free(new);
 		return (0);
-	}
 	new->key = (char *)key;
-	new->value = val_clone;
+	new->value = strdup(value);
+	new->next = NULL;
 
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new;
-		new->next = NULL;
 		return (1);
 	}
+
+	current = ht->array[index];
+	for (; current != NULL; current = current->next)
+		if (strcmp(current->key, (char *)key))
+		{
+			current->value = (char *)value;
+			free(new->key);
+			free(new->value);
+			free(new);
+			return (1);
+		}
 	new->next = ht->array[index];
 	ht->array[index] = new;
 	return (1);
